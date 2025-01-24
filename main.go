1e905ea7b021bc2a9ff8992ef576e702a6046b07
Artur Manukian <manukian.artur.94@gmail.com>
2025-01-24 19:37:41 +0200

    feat: Ch 1, L 3

--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	trimmed := strings.TrimSpace(text)
+	splitted := strings.Split(trimmed, " ")
+	filtered := []string{}
+
+	for _, s := range splitted {
+		if s == "" {
+			continue
+		}
+
+		filtered = append(filtered, s)
+	}
+
+	return filtered
+}
 
 func main() {
 	fmt.Println("Hello, World!")
---
+++ repl_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "hello world",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "hello  world",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+
+		if len(actual) != len(c.expected) {
+			t.Errorf("cleanInput(%q) == %q, want %q", c.input, actual, c.expected)
+			continue
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+
+			if word != expectedWord {
+				t.Errorf("wanted %s, got %s", expectedWord, word)
+			}
+		}
+	}
+}
